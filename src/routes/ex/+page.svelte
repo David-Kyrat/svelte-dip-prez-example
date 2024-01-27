8e2155e7c935a485aa8596a46436f3eaafd9a700
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2024-01-27 22:28:28 +0100

    Added ex dir

---
+++ src/routes/ex/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Slides from '$lib/deck/slides.svelte'
+	import '../app.postcss'
+	import Presentation from '$lib/deck/presentation.svelte'
+</script>
+
+<svelte:head>
+	<title>Presentation</title>
+</svelte:head>
+
+<h1 class="w-fit text-2xl block mx-auto mt-5 font-bold text-white">Test</h1>
+<!-- <Slides /> -->
+
+<Slides>
+	<Presentation />
+</Slides>
