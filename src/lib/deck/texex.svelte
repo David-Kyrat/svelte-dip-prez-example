5ca326bdbacab932ee956d06dc8f8ca639ecacbe
David Kyrat <33601938+David-Kyrat@users.noreply.github.com>
2024-01-28 17:48:14 +0100

    renamed to presMd

--- src/lib/deck/texex.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import Slide from './slide.svelte'
-	import Code from './code.svelte'
-	import Markdown from './markdown.svelte'
-</script>
-
-<Markdown name="/example.md" external clas="" />
--- src/routes/q1/+page.svelte
+++ src/routes/q1/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-  import PrezMd from '$lib/prezMd.svelte';
+  import PresMd from '$lib/presMd.svelte';
 	import '$lib/app.postcss'
 </script>
 
-<PrezMd name="/example.md" title="Q1 Example"  />
+<PresMd name="/example.md" title="Q1 Example"  />
